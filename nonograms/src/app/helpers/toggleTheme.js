0e7d89414bf67fcf1eb610823fae90b59a3272b2
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-10-24 23:37:24 +0300

    feat: add gh-pages bundle

--- nonograms/src/app/helpers/toggleTheme.js
+++
@@ -1,7 +0,0 @@
-export default function toggleTheme() {
-  document.body.classList.toggle('dark-theme');
-  document.querySelectorAll('.button').forEach((btn) => {
-    btn.classList.toggle('is-white');
-    btn.classList.toggle('is-dark');
-  });
-}
--- nonograms/src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-import './main.scss';
-
-import App from './app/app';
-
-const app = new App();
